c5465d91ffd6247a4544f0a71a742bc26fb556e1
Premysl Fara <premysl.fara@gmail.com>
2024-01-21 20:01:28 +0100

    GL debugging callback.

--- src/MicroEngineDemoApp/GameWindow.cs
+++ src/MicroEngineDemoApp/GameWindow.cs
@@ -37,6 +37,17 @@ public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
         
         _viewportSizeScaleFactor = Program.Settings.ViewportSizeScaleFactor;
         
+        #if DEBUG
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) == false)
+        {
+            GL.DebugMessageCallback(DebugMessageDelegate, IntPtr.Zero);
+            GL.Enable(EnableCap.DebugOutput);
+            //GL.Enable(EnableCap.DebugOutputSynchronous);
+        }
+
+        #endif
+        
         GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         GL.Enable(EnableCap.DepthTest);
         
@@ -95,4 +106,39 @@ public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
         GL.Viewport(0, 0, Size.X, Size.Y);
         _game.SetCameraAspectRatio(Size.X / (float)Size.Y);
     }
+    
+    
+    #if DEBUG
+    
+    // https://opentk.net/learn/appendix_opengl/debug_callback.html?tabs=debug-context-4%2Cdelegate-gl%2Cenable-gl
+
+    private static DebugProc DebugMessageDelegate = OnDebugMessage;
+
+    private static void OnDebugMessage(
+        DebugSource source,     // Source of the debugging message.
+        DebugType type,         // Type of the debugging message.
+        int id,                 // ID associated with the message.
+        DebugSeverity severity, // Severity of the message.
+        int length,             // Length of the string in pMessage.
+        IntPtr pMessage,        // Pointer to message string.
+        IntPtr pUserParam)      // The pointer you gave to OpenGL, explained later.
+    {
+        // In order to access the string pointed to by pMessage, you can use Marshal
+        // class to copy its contents to a C# string without unsafe code. You can
+        // also use the new function Marshal.PtrToStringUTF8 since .NET Core 1.1.
+        var message = Marshal.PtrToStringAnsi(pMessage, length);
+
+        // The rest of the function is up to you to implement, however a debug output
+        // is always useful.
+        Console.WriteLine("[{0} source={1} type={2} id={3}] {4}", severity, source, type, id, message);
+
+        // Potentially, you may want to throw from the function for certain severity
+        // messages.
+        if (type == DebugType.DebugTypeError)
+        {
+            throw new Exception(message);
+        }
+    }
+    
+    #endif
 }
--- src/MicroEngineDemoApp/Program.cs
+++ src/MicroEngineDemoApp/Program.cs
@@ -9,10 +9,8 @@ using Microsoft.Extensions.FileProviders.Physical;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
-using OpenTK.Graphics.OpenGL4;
 
 using MicroEngineDemoApp.Games;
-using System.Runtime.InteropServices;
 
 internal static class Program
 {
@@ -92,35 +90,4 @@ internal static class Program
         Settings = config.GetSection("Settings")
             .Get<Settings>() ?? Settings.DefaultSettings;
     }
-
-
-    // // https://opentk.net/learn/appendix_opengl/debug_callback.html?tabs=debug-context-4%2Cdelegate-gl%2Cenable-gl
-
-    // private static GLDebugProc DebugMessageDelegate = OnDebugMessage;
-
-    // private static void OnDebugMessage(
-    //     DebugSource source,     // Source of the debugging message.
-    //     DebugType type,         // Type of the debugging message.
-    //     int id,                 // ID associated with the message.
-    //     DebugSeverity severity, // Severity of the message.
-    //     int length,             // Length of the string in pMessage.
-    //     IntPtr pMessage,        // Pointer to message string.
-    //     IntPtr pUserParam)      // The pointer you gave to OpenGL, explained later.
-    // {
-    //     // In order to access the string pointed to by pMessage, you can use Marshal
-    //     // class to copy its contents to a C# string without unsafe code. You can
-    //     // also use the new function Marshal.PtrToStringUTF8 since .NET Core 1.1.
-    //     string message = Marshal.PtrToStringAnsi(pMessage, length);
-
-    //     // The rest of the function is up to you to implement, however a debug output
-    //     // is always useful.
-    //     Console.WriteLine("[{0} source={1} type={2} id={3}] {4}", severity, source, type, id, message);
-
-    //     // Potentially, you may want to throw from the function for certain severity
-    //     // messages.
-    //     if (type == DebugType.DebugTypeError)
-    //     {
-    //         throw new Exception(message);
-    //     }
-    // }
 }
