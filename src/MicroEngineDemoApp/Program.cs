a0f673c2e457df7e0687e291a5ba45ed1d0ea09d
enif77 <premysl.fara@gmail.com>
2024-01-21 13:13:14 +0100

    GL debug base.

--- src/MicroEngineDemoApp/Program.cs
+++ src/MicroEngineDemoApp/Program.cs
@@ -9,8 +9,10 @@ using Microsoft.Extensions.FileProviders.Physical;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
 
 using MicroEngineDemoApp.Games;
+using System.Runtime.InteropServices;
 
 internal static class Program
 {
@@ -33,7 +35,7 @@ internal static class Program
             Title = Defaults.AppVersionInfo,
             
             // This is needed to run on macos
-            Flags = ContextFlags.ForwardCompatible,
+            Flags = ContextFlags.ForwardCompatible | ContextFlags.Debug,
             Vsync = Settings.EnableVSync ? VSyncMode.Adaptive : VSyncMode.Off,
             WindowState = Settings.EnableFullscreen ? WindowState.Fullscreen : WindowState.Normal
         };
@@ -90,4 +92,35 @@ internal static class Program
         Settings = config.GetSection("Settings")
             .Get<Settings>() ?? Settings.DefaultSettings;
     }
+
+
+    // // https://opentk.net/learn/appendix_opengl/debug_callback.html?tabs=debug-context-4%2Cdelegate-gl%2Cenable-gl
+
+    // private static GLDebugProc DebugMessageDelegate = OnDebugMessage;
+
+    // private static void OnDebugMessage(
+    //     DebugSource source,     // Source of the debugging message.
+    //     DebugType type,         // Type of the debugging message.
+    //     int id,                 // ID associated with the message.
+    //     DebugSeverity severity, // Severity of the message.
+    //     int length,             // Length of the string in pMessage.
+    //     IntPtr pMessage,        // Pointer to message string.
+    //     IntPtr pUserParam)      // The pointer you gave to OpenGL, explained later.
+    // {
+    //     // In order to access the string pointed to by pMessage, you can use Marshal
+    //     // class to copy its contents to a C# string without unsafe code. You can
+    //     // also use the new function Marshal.PtrToStringUTF8 since .NET Core 1.1.
+    //     string message = Marshal.PtrToStringAnsi(pMessage, length);
+
+    //     // The rest of the function is up to you to implement, however a debug output
+    //     // is always useful.
+    //     Console.WriteLine("[{0} source={1} type={2} id={3}] {4}", severity, source, type, id, message);
+
+    //     // Potentially, you may want to throw from the function for certain severity
+    //     // messages.
+    //     if (type == DebugType.DebugTypeError)
+    //     {
+    //         throw new Exception(message);
+    //     }
+    // }
 }
