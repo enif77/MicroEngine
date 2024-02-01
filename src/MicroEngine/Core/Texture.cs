fc01810163cd5163286d73d5d6acad266de49acd
enif77 <premysl.fara@gmail.com>
2024-02-01 17:06:06 +0100

    Texture can be loaded from a RGBA array of bytes.

--- src/MicroEngine/Core/Texture.cs
+++ src/MicroEngine/Core/Texture.cs
@@ -12,41 +12,28 @@ public class Texture : ITexture
 {
     public readonly int Handle;
 
-    
-    public static Texture LoadFromFile(string path, TextureWrapMode wrapMode = TextureWrapMode.Repeat)
+
+    public static Texture LoadFromRgbaBytes(byte[] bytes, int width, int height, TextureWrapMode wrapMode = TextureWrapMode.Repeat)
     {
-        // Generate handle
         var handle = GL.GenTexture();
 
         // Bind the handle
         GL.ActiveTexture(TextureUnit.Texture0);
         GL.BindTexture(TextureTarget.Texture2D, handle);
-
-        // For this example, we're going to use .NET's built-in System.Drawing library to load textures.
-
-        // OpenGL has it's texture origin in the lower left corner instead of the top left corner,
-        // so we tell StbImageSharp to flip the image when loading.
-        StbImage.stbi_set_flip_vertically_on_load(1);
-            
-        // Here we open a stream to the file and pass it to StbImageSharp to load.
-        using (Stream stream = File.OpenRead(path))
-        {
-            var image = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
-
-            // Now that our pixels are prepared, it's time to generate a texture. We do this with GL.TexImage2D.
-            // Arguments:
-            //   The type of texture we're generating. There are various different types of textures, but the only one we need right now is Texture2D.
-            //   Level of detail. We can use this to start from a smaller mipmap (if we want), but we don't need to do that, so leave it at 0.
-            //   Target format of the pixels. This is the format OpenGL will store our image with.
-            //   Width of the image
-            //   Height of the image.
-            //   Border of the image. This must always be 0; it's a legacy parameter that Khronos never got rid of.
-            //   The format of the pixels, explained above. Since we loaded the pixels as RGBA earlier, we need to use PixelFormat.Rgba.
-            //   Data type of the pixels.
-            //   And finally, the actual pixels.
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
-        }
-            
+        
+        // Now that our pixels are prepared, it's time to generate a texture. We do this with GL.TexImage2D.
+        // Arguments:
+        //   The type of texture we're generating. There are various different types of textures, but the only one we need right now is Texture2D.
+        //   Level of detail. We can use this to start from a smaller mipmap (if we want), but we don't need to do that, so leave it at 0.
+        //   Target format of the pixels. This is the format OpenGL will store our image with.
+        //   Width of the image
+        //   Height of the image.
+        //   Border of the image. This must always be 0; it's a legacy parameter that Khronos never got rid of.
+        //   The format of the pixels, explained above. Since we loaded the pixels as RGBA earlier, we need to use PixelFormat.Rgba.
+        //   Data type of the pixels.
+        //   And finally, the actual pixels.
+        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, bytes);
+        
         // Now that our texture is loaded, we can set a few settings to affect how the image appears on rendering.
 
         // First, we set the min and mag filter. These are used for when the texture is scaled down and up, respectively.
@@ -72,6 +59,22 @@ public class Texture : ITexture
         GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 
         return new Texture(handle);
+    }
+
+    
+    public static Texture LoadFromFile(string path, TextureWrapMode wrapMode = TextureWrapMode.Repeat)
+    {
+        // OpenGL has it's texture origin in the lower left corner instead of the top left corner,
+        // so we tell StbImageSharp to flip the image when loading.
+        StbImage.stbi_set_flip_vertically_on_load(1);
+            
+        // Here we open a stream to the file and pass it to StbImageSharp to load.
+        using (var stream = File.OpenRead(path))
+        {
+            var image = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
+
+            return LoadFromRgbaBytes(image.Data, image.Width, image.Height, wrapMode);
+        }
     }
 
 
