b319231d735acc12a26692d8a83ca1037b9392b6
Premysl Fara <premysl.fara@gmail.com>
2024-02-06 11:56:58 +0100

    Better multi texture geometry examples.

--- src/MicroEngine/Geometries/MultiTextureIndexedGeometry.cs
+++ src/MicroEngine/Geometries/MultiTextureIndexedGeometry.cs
@@ -7,7 +7,7 @@ using OpenTK.Graphics.OpenGL4;
 using MicroEngine.Extensions;
 
 /// <summary>
-/// A geometry with multiple textures and indices. No normals.
+/// A geometry with multiple textures and indices. No lights.
 /// </summary>
 public class MultiTextureIndexedGeometry : GeometryBase
 {
@@ -56,56 +56,49 @@ public class MultiTextureIndexedGeometry : GeometryBase
 /*
 
 Example of a cube geometry:
- 
- Each side has 2 triangles, each triangle has 3 vertices.
-   
- verices = [  
-   // Positions          Texture coords
+
+vertices = [
+   // Positions          Texture ID and coords
    -0.5f, -0.5f, -0.5f,  0,  0.0f, 0.0f,  // Front face.
     0.5f, -0.5f, -0.5f,  0,  1.0f, 0.0f,
     0.5f,  0.5f, -0.5f,  0,  1.0f, 1.0f,
-    0.5f,  0.5f, -0.5f,  0,  1.0f, 1.0f,
    -0.5f,  0.5f, -0.5f,  0,  0.0f, 1.0f,
-   -0.5f, -0.5f, -0.5f,  0,  0.0f, 0.0f,
 
     0.5f, -0.5f, -0.5f,  1,  0.0f, 0.0f,  // Right face.
     0.5f, -0.5f,  0.5f,  1,  1.0f, 0.0f,
     0.5f,  0.5f,  0.5f,  1,  1.0f, 1.0f,
-    0.5f,  0.5f,  0.5f,  1,  1.0f, 1.0f,
     0.5f,  0.5f, -0.5f,  1,  0.0f, 1.0f,
-    0.5f, -0.5f, -0.5f,  1,  0.0f, 0.0f,
    
     0.5f, -0.5f,  0.5f,  2,  0.0f, 0.0f,  // Back face.
    -0.5f, -0.5f,  0.5f,  2,  1.0f, 0.0f,
    -0.5f,  0.5f,  0.5f,  2,  1.0f, 1.0f,
-   -0.5f,  0.5f,  0.5f,  2,  1.0f, 1.0f,
     0.5f,  0.5f,  0.5f,  2,  0.0f, 1.0f,
-    0.5f, -0.5f,  0.5f,  2,  0.0f, 0.0f,
 
    -0.5f, -0.5f,  0.5f,  3,  0.0f, 0.0f,  // Left face.
    -0.5f, -0.5f, -0.5f,  3,  1.0f, 0.0f,
    -0.5f,  0.5f, -0.5f,  3,  1.0f, 1.0f,
-   -0.5f,  0.5f, -0.5f,  3,  1.0f, 1.0f,
    -0.5f,  0.5f,  0.5f,  3,  0.0f, 1.0f,
-   -0.5f, -0.5f,  0.5f,  3,  0.0f, 0.0f,
 
    -0.5f,  0.5f, -0.5f,  4,  0.0f, 0.0f,  // Top face.
     0.5f,  0.5f, -0.5f,  4,  1.0f, 0.0f,
     0.5f,  0.5f,  0.5f,  4,  1.0f, 1.0f,
-    0.5f,  0.5f,  0.5f,  4,  1.0f, 1.0f,
    -0.5f,  0.5f,  0.5f,  4,  0.0f, 1.0f,
-   -0.5f,  0.5f, -0.5f,  4,  0.0f, 0.0f,
    
    -0.5f, -0.5f,  0.5f,  5,  0.0f, 0.0f,  // Bottom face.
     0.5f, -0.5f,  0.5f,  5,  1.0f, 0.0f,
     0.5f, -0.5f, -0.5f,  5,  1.0f, 1.0f,
-    0.5f, -0.5f, -0.5f,  5,  1.0f, 1.0f,
    -0.5f, -0.5f, -0.5f,  5,  0.0f, 1.0f,
-   -0.5f, -0.5f,  0.5f,  5,  0.0f, 0.0f,
 ];
-       
-// 36 = 6 sides * 2 triangles per side * 3 vertices per triangle.
-indices-count = 36;
-  
+   
+indices = [
+   // Each side has 2 triangles, each triangle has 3 vertices.
+    0,  1,  2,   2,  3,  0, // Front
+    4,  5,  6,   6,  7,  4, // Right
+    8,  9, 10,  10, 11,  8, // Back
+   12, 13, 14,  14, 15, 12, // Left
+   16, 17, 18,  18, 19, 16, // Top
+   20, 21, 22,  22, 23, 20  // Bottom
+];   
+
 */
  
