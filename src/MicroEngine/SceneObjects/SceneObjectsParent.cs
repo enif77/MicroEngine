c60164096d5a8d4ecba3ba63f49839ff75d2ff32
Premysl Fara <premysl.fara@gmail.com>
2024-02-26 10:39:14 +0100

    SceneObjectsParent added.

---
+++ src/MicroEngine/SceneObjects/SceneObjectsParent.cs
@@ -0,0 +1,11 @@
+/* Copyright (C) Premysl Fara and Contributors */
+
+namespace MicroEngine.SceneObjects;
+
+/// <summary>
+/// A parent scene object for a group of scene objects.
+/// </summary>
+public class SceneObjectsParent : SceneObjectBase
+{
+    // Nothing here, because this class is just a marker for a group of scene objects.
+}
