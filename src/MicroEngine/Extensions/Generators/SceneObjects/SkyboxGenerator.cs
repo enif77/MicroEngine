ec5ce09ca967db725599ff9f0a8f8a14345ad09a
Premysl Fara <premysl.fara@gmail.com>
2024-02-26 09:28:53 +0100

    Skybox generator can generate geometry only.

--- src/MicroEngine/Extensions/Generators/SceneObjects/SkyboxGenerator.cs
+++ src/MicroEngine/Extensions/Generators/SceneObjects/SkyboxGenerator.cs
@@ -18,50 +18,57 @@ public static class SkyboxGenerator
     /// <exception cref="ArgumentNullException">Thrown, if the material argument is null.</exception>
     public static ISceneObject Generate(IMaterial material)
     {
-        return new GenericSkybox(new MultiTextureIndexedGeometry(
-            [
-                // Positions          Texture ID and coords
-                -0.5f, -0.5f, -0.5f,  0,  0.0f, 0.0f,  // Front face.
-                0.5f, -0.5f, -0.5f,  0,  1.0f, 0.0f,
-                0.5f,  0.5f, -0.5f,  0,  1.0f, 1.0f,
-                -0.5f,  0.5f, -0.5f,  0,  0.0f, 1.0f,
-        
-                0.5f, -0.5f, -0.5f,  1,  0.0f, 0.0f,  // Right face.
-                0.5f, -0.5f,  0.5f,  1,  1.0f, 0.0f,
-                0.5f,  0.5f,  0.5f,  1,  1.0f, 1.0f,
-                0.5f,  0.5f, -0.5f,  1,  0.0f, 1.0f,
-            
-                0.5f, -0.5f,  0.5f,  2,  0.0f, 0.0f,  // Back face.
-                -0.5f, -0.5f,  0.5f,  2,  1.0f, 0.0f,
-                -0.5f,  0.5f,  0.5f,  2,  1.0f, 1.0f,
-                0.5f,  0.5f,  0.5f,  2,  0.0f, 1.0f,
-        
-                -0.5f, -0.5f,  0.5f,  3,  0.0f, 0.0f,  // Left face.
-                -0.5f, -0.5f, -0.5f,  3,  1.0f, 0.0f,
-                -0.5f,  0.5f, -0.5f,  3,  1.0f, 1.0f,
-                -0.5f,  0.5f,  0.5f,  3,  0.0f, 1.0f,
-        
-                -0.5f,  0.5f, -0.5f,  4,  0.0f, 0.0f,  // Top face.
-                0.5f,  0.5f, -0.5f,  4,  1.0f, 0.0f,
-                0.5f,  0.5f,  0.5f,  4,  1.0f, 1.0f,
-                -0.5f,  0.5f,  0.5f,  4,  0.0f, 1.0f,
-            
-                -0.5f, -0.5f,  0.5f,  5,  0.0f, 0.0f,  // Bottom face.
-                0.5f, -0.5f,  0.5f,  5,  1.0f, 0.0f,
-                0.5f, -0.5f, -0.5f,  5,  1.0f, 1.0f,
-                -0.5f, -0.5f, -0.5f,  5,  0.0f, 1.0f,
-            ],
-            [
-                // Each side has 2 triangles, each triangle has 3 vertices.
-                0,  1,  2,   2,  3,  0, // Front
-                4,  5,  6,   6,  7,  4, // Right
-                8,  9, 10,  10, 11,  8, // Back
-                12, 13, 14,  14, 15, 12, // Left
-                16, 17, 18,  18, 19, 16, // Top
-                20, 21, 22,  22, 23, 20  // Bottom
-            ]))
+        return new GenericSkybox(GenerateSkyboxGeometry())
         {
             Material = material ?? throw new ArgumentNullException(nameof(material))
         };
     }
+
+    /// <summary>
+    /// Generates a geometry for a skybox-like object with a per-side textures.
+    /// </summary>
+    /// <returns>A geometry for a skybox-like object with a per-side textures.</returns>
+    public static IGeometry GenerateSkyboxGeometry()
+        => new MultiTextureIndexedGeometry(
+            [
+                // Positions          Texture ID and coords
+                -0.5f, -0.5f, -0.5f,  0,  0.0f, 0.0f, // Front face.
+                 0.5f, -0.5f, -0.5f,  0,  1.0f, 0.0f,
+                 0.5f,  0.5f, -0.5f,  0,  1.0f, 1.0f,
+                -0.5f,  0.5f, -0.5f,  0,  0.0f, 1.0f,
+
+                 0.5f, -0.5f, -0.5f,  1,  0.0f, 0.0f, // Right face.
+                 0.5f, -0.5f,  0.5f,  1,  1.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  1,  1.0f, 1.0f,
+                 0.5f,  0.5f, -0.5f,  1,  0.0f, 1.0f,
+
+                 0.5f, -0.5f,  0.5f,  2,  0.0f, 0.0f, // Back face.
+                -0.5f, -0.5f,  0.5f,  2,  1.0f, 0.0f,
+                -0.5f,  0.5f,  0.5f,  2,  1.0f, 1.0f,
+                 0.5f,  0.5f,  0.5f,  2,  0.0f, 1.0f,
+
+                -0.5f, -0.5f,  0.5f,  3,  0.0f, 0.0f, // Left face.
+                -0.5f, -0.5f, -0.5f,  3,  1.0f, 0.0f,
+                -0.5f,  0.5f, -0.5f,  3,  1.0f, 1.0f,
+                -0.5f,  0.5f,  0.5f,  3,  0.0f, 1.0f,
+
+                -0.5f,  0.5f, -0.5f,  4,  0.0f, 0.0f, // Top face.
+                 0.5f,  0.5f, -0.5f,  4,  1.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  4,  1.0f, 1.0f,
+                -0.5f,  0.5f,  0.5f,  4,  0.0f, 1.0f,
+
+                -0.5f, -0.5f,  0.5f,  5,  0.0f, 0.0f, // Bottom face.
+                 0.5f, -0.5f,  0.5f,  5,  1.0f, 0.0f,
+                 0.5f, -0.5f, -0.5f,  5,  1.0f, 1.0f,
+                -0.5f, -0.5f, -0.5f,  5,  0.0f, 1.0f,
+            ],
+            [
+                // Each side has 2 triangles, each triangle has 3 vertices.
+                 0,  1,  2,   2,  3,  0, // Front
+                 4,  5,  6,   6,  7,  4, // Right
+                 8,  9, 10,  10, 11,  8, // Back
+                12, 13, 14,  14, 15, 12, // Left
+                16, 17, 18,  18, 19, 16, // Top
+                20, 21, 22,  22, 23, 20  // Bottom
+            ]);
 }
