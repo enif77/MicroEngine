73b2135956b89473550b6e0801174db546f51b91
Premysl Fara <premysl.fara@gmail.com>
2024-03-03 12:45:34 +0100

    Scene object parent renamed to scene object group.

--- src/MicroEngine/SceneObjects/SceneObjectsGroup.cs
+++ src/MicroEngine/SceneObjects/SceneObjectsGroup.cs
@@ -5,7 +5,7 @@ namespace MicroEngine.SceneObjects;
 /// <summary>
 /// A parent scene object for a group of scene objects.
 /// </summary>
-public class SceneObjectsParent : SceneObjectBase
+public class SceneObjectsGroup : SceneObjectBase
 {
     // Nothing here, because this class is just a marker for a group of scene objects.
 }
