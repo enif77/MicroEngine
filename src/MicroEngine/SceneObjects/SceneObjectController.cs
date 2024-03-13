534e48a6c3821052ee3cddac4e60414f13cf30d6
Premysl Fara <premysl.fara@gmail.com>
2024-03-13 18:49:42 +0100

    Vectors are protected.

--- src/MicroEngine/SceneObjects/SceneObjectController.cs
+++ src/MicroEngine/SceneObjects/SceneObjectController.cs
@@ -13,9 +13,9 @@ using MicroEngine.Extensions;
 /// </summary>
 public class SceneObjectController : SceneObjectBase
 {
-    private Vector3 _frontVector = -Vector3.UnitZ;
-    private Vector3 _upVector = Vector3.UnitY;
-    private Vector3 _rightVector = Vector3.UnitX;
+    protected Vector3 FrontVector = -Vector3.UnitZ;
+    protected Vector3 UpVector = Vector3.UnitY;
+    protected Vector3 RightVector = Vector3.UnitX;
     
     
     /// <summary>
@@ -31,9 +31,9 @@ public class SceneObjectController : SceneObjectBase
         Rotation = Vector3.Zero;
         
         // Reset the axes.
-        _frontVector = -Vector3.UnitZ;
-        _upVector = Vector3.UnitY;
-        _rightVector = Vector3.UnitX;
+        FrontVector = -Vector3.UnitZ;
+        UpVector = Vector3.UnitY;
+        RightVector = Vector3.UnitX;
         
         Roll(thisRotation.Z);
         Pitch(thisRotation.X);
@@ -48,10 +48,10 @@ public class SceneObjectController : SceneObjectBase
     {
         this.SetRotationY(Rotation.Y + angle);
         
-        var m = Matrix4.CreateFromAxisAngle(_upVector, angle);
+        var m = Matrix4.CreateFromAxisAngle(UpVector, angle);
         
-        _rightVector = Vector3.TransformVector(_rightVector, m);
-        _frontVector = Vector3.TransformVector(_frontVector, m);
+        RightVector = Vector3.TransformVector(RightVector, m);
+        FrontVector = Vector3.TransformVector(FrontVector, m);
     }
     
     /// <summary>
@@ -62,10 +62,10 @@ public class SceneObjectController : SceneObjectBase
     {
         this.SetRotationX(Rotation.X + angle);
         
-        var m = Matrix4.CreateFromAxisAngle(_rightVector, angle);
+        var m = Matrix4.CreateFromAxisAngle(RightVector, angle);
         
-        _upVector = Vector3.TransformVector(_upVector, m);
-        _frontVector = Vector3.TransformVector(_frontVector, m);
+        UpVector = Vector3.TransformVector(UpVector, m);
+        FrontVector = Vector3.TransformVector(FrontVector, m);
     }
 
     /// <summary>
@@ -76,10 +76,10 @@ public class SceneObjectController : SceneObjectBase
     {
         this.SetRotationZ(Rotation.Z + angle);
         
-        var m = Matrix4.CreateFromAxisAngle(_frontVector, angle); 
+        var m = Matrix4.CreateFromAxisAngle(FrontVector, angle); 
         
-        _rightVector = Vector3.TransformVector(_rightVector, m);
-        _upVector = Vector3.TransformVector(_upVector, m);
+        RightVector = Vector3.TransformVector(RightVector, m);
+        UpVector = Vector3.TransformVector(UpVector, m);
     }
     
     /// <summary>
@@ -88,7 +88,7 @@ public class SceneObjectController : SceneObjectBase
     /// <param name="distance">How far this camera should move from its current position.</param>
     public void Advance(float distance)
     {
-        Position += _frontVector * distance;
+        Position += FrontVector * distance;
     }
 
     /// <summary>
@@ -97,7 +97,7 @@ public class SceneObjectController : SceneObjectBase
     /// <param name="distance">How far this camera should move from its current position.</param>
     public void Ascend(float distance)
     {
-        Position += _upVector * -distance;
+        Position += UpVector * -distance;
     }
 
     /// <summary>
@@ -106,7 +106,7 @@ public class SceneObjectController : SceneObjectBase
     /// <param name="distance">How far this camera should move from its current position.</param>
     public void Strafe(float distance)
     {
-        Position += _rightVector * -distance;
+        Position += RightVector * -distance;
     }
     
     
@@ -117,19 +117,19 @@ public class SceneObjectController : SceneObjectBase
             var modelMatrix = Matrix4.CreateScale(Scale);
            
             // right   = glm::vec3(matrix[0][0], matrix[0][1], matrix[0][2]);
-            modelMatrix.M11 = -_rightVector.X;
-            modelMatrix.M12 = -_rightVector.Y;
-            modelMatrix.M13 = -_rightVector.Z;
+            modelMatrix.M11 = -RightVector.X;
+            modelMatrix.M12 = -RightVector.Y;
+            modelMatrix.M13 = -RightVector.Z;
 
             // up      = glm::vec3(matrix[1][0], matrix[1][1], matrix[1][2]);
-            modelMatrix.M21 = -_upVector.X;
-            modelMatrix.M22 = -_upVector.Y;
-            modelMatrix.M23 = -_upVector.Z;
+            modelMatrix.M21 = -UpVector.X;
+            modelMatrix.M22 = -UpVector.Y;
+            modelMatrix.M23 = -UpVector.Z;
 
             // forward = glm::vec3(matrix[2][0], matrix[2][1], matrix[2][2]);
-            modelMatrix.M31 = -_frontVector.X;
-            modelMatrix.M32 = -_frontVector.Y;
-            modelMatrix.M33 = -_frontVector.Z;
+            modelMatrix.M31 = -FrontVector.X;
+            modelMatrix.M32 = -FrontVector.Y;
+            modelMatrix.M33 = -FrontVector.Z;
 
             modelMatrix *= Matrix4.CreateTranslation(Position);
         
