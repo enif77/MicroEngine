2bf841b6646f01296c66cf52dc6a8327e68e1516
Premysl Fara <premysl.fara@gmail.com>
2024-03-31 09:38:10 +0200

    Static AABB fixes.

--- src/MicroEngine/SceneObjects/StaticAxisAlignedBoundaryBox.cs
+++ src/MicroEngine/SceneObjects/StaticAxisAlignedBoundaryBox.cs
@@ -10,27 +10,35 @@ using OpenTK.Mathematics;
 /// </summary>
 public class StaticAxisAlignedBoundaryBox : SceneObjectBase, IAxisAlignedBoundaryBox
 {
-    public Vector3 Min { get; private set; } = new Vector3(-0.5f, -0.5f, -0.5f);
-    public Vector3 Max { get; private set; } = new Vector3(0.5f, 0.5f, 0.5f);
+    private static readonly Vector3 MinVertex = new Vector3(-0.5f, -0.5f, -0.5f);
+    private static readonly Vector3 MaxVertex = new Vector3(0.5f, 0.5f, 0.5f);
+    
+    public Vector3 Min { get; private set; } = MinVertex;
+    public Vector3 Max { get; private set; } = MaxVertex;
     
     
     public override void Update(float deltaTime)
     {
         if (NeedsModelMatrixUpdate)
         {
+            // We are updating the geometry using world coordinates,
+            // so we do not need any transformations here.
+            // Note: This breaks the scene graph hierarchy, but we need to do it this way.
+            ModelMatrix = Matrix4.Identity;
+            
             // Update the ModelMatrix, ignoring the rotation.
-            ModelMatrix = Matrix4.CreateScale(Scale);
-            ModelMatrix *= Matrix4.CreateTranslation(Position);
+            var worldMatrix = Matrix4.CreateScale(Scale);
+            worldMatrix *= Matrix4.CreateTranslation(Position);
         
             if (Parent != null)
             {
                 // Combine with the scale and the translation of the parent scene object.
-                ModelMatrix *= Parent.ModelMatrix.ClearRotation();
+                worldMatrix *= Parent.ModelMatrix.ClearRotation();
             }
             
             // Update the Min and Max values.
-            Min = Vector3.TransformPosition(new Vector3(-0.5f, -0.5f, -0.5f), ModelMatrix);
-            Max = Vector3.TransformPosition(new Vector3(0.5f, 0.5f, 0.5f), ModelMatrix);
+            Min = Vector3.TransformPosition(MinVertex, worldMatrix);
+            Max = Vector3.TransformPosition(MaxVertex, worldMatrix);
             
             NeedsModelMatrixUpdate = false;
         }
