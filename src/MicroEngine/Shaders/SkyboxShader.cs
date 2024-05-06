ee07ac10cbf06ee79bdc5bd0f4a8e25bcbe51a85
Premysl Fara <premysl.fara@gmail.com>
2024-05-06 11:51:21 +0200

    Skybox shader uses embedded GLSL sources.

--- src/MicroEngine/Shaders/SkyboxShader.cs
+++ src/MicroEngine/Shaders/SkyboxShader.cs
@@ -15,9 +15,46 @@ public class SkyboxShader : IShader
     {
         ArgumentNullException.ThrowIfNull(resourcesManager);
         
+        // _shader = new Shader(
+        //     resourcesManager.LoadTextFile("Shaders/skybox.vert"),
+        //     resourcesManager.LoadTextFile("Shaders/skybox.frag"));
+        
         _shader = new Shader(
-            resourcesManager.LoadTextFile("Shaders/skybox.vert"),
-            resourcesManager.LoadTextFile("Shaders/skybox.frag"));
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            layout (location = 0) in vec3 aPos;
+            layout (location = 1) in vec2 aTexCoords;
+            
+            uniform mat4 model;
+            uniform mat4 view;
+            uniform mat4 projection;
+            
+            out vec2 TexCoords;
+            
+            void main()
+            {
+                vec4 pos = vec4(aPos, 1.0) * model * view * projection;
+                gl_Position = pos.xyww;  // This forces the z value to be 1.0.
+                TexCoords = aTexCoords;
+            }
+            """,
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            uniform sampler2D texture0;
+            
+            in vec2 TexCoords;
+            
+            out vec4 FragColor;
+            
+            void main()
+            {
+                FragColor = texture(texture0, TexCoords);
+            } 
+            """);
     }
 
     
