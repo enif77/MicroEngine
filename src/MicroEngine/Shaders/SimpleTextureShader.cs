2a868c04b69aa04c42702388f19f9188a5a62178
Premysl Fara <premysl.fara@gmail.com>
2024-05-06 12:02:01 +0200

    Simple texture shader uses embedded shader sources.

--- src/MicroEngine/Shaders/SimpleTextureShader.cs
+++ src/MicroEngine/Shaders/SimpleTextureShader.cs
@@ -16,8 +16,47 @@ public class SimpleTextureShader : IShader
         ArgumentNullException.ThrowIfNull(resourcesManager);
         
         _shader = new Shader(
-            resourcesManager.LoadTextFile("Shaders/simple-texture.vert"),
-            resourcesManager.LoadTextFile("Shaders/simple-texture.frag"));
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            layout (location = 0) in vec3 aPos;
+            layout (location = 1) in vec2 aTexCoords;
+            
+            uniform mat4 model;
+            uniform mat4 view;
+            uniform mat4 projection;
+            
+            out vec2 TexCoords;
+            
+            void main()
+            {
+                gl_Position = vec4(aPos, 1.0) * model * view * projection;
+                TexCoords = aTexCoords;
+            }
+            """,
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            uniform sampler2D texture0;
+            
+            in vec2 TexCoords;
+            
+            out vec4 FragColor;
+            
+            void main()
+            {
+                //FragColor = texture(texture0, TexCoords);
+                vec4 texColor = texture(texture0, TexCoords);
+                if (texColor.a < 0.1)
+                {
+                    discard;
+                }
+            
+                FragColor = texColor;
+            }
+            """);
     }
 
     
