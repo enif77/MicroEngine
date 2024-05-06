fec5a2b17524056d3509b054426547f9dd683c35
Premysl Fara <premysl.fara@gmail.com>
2024-05-06 12:32:42 +0200

    Multi texture shaders are using embedded shader sources.

--- src/MicroEngine/Shaders/MultiTextureShader.cs
+++ src/MicroEngine/Shaders/MultiTextureShader.cs
@@ -8,8 +8,57 @@ public class MultiTextureShader : MultiTextureShaderBase
 {
     public MultiTextureShader(IResourcesManager resourcesManager)
         : base(new Shader(
-            resourcesManager.LoadTextFile("Shaders/multi-texture.vert"),
-            resourcesManager.LoadTextFile("Shaders/multi-texture.frag")))
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            layout (location = 0) in vec3 aPos;
+            layout (location = 1) in float aTexId;
+            layout (location = 2) in vec2 aTexCoords;
+            
+            uniform mat4 model;
+            uniform mat4 view;
+            uniform mat4 projection;
+            
+            flat out int TexId;
+            out vec2 TexCoords;
+            
+            void main()
+            {
+                gl_Position = vec4(aPos, 1.0) * model * view * projection;
+                TexId = int(aTexId);
+                TexCoords = aTexCoords;
+            }
+            """,
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            struct Sampler
+            {
+                sampler2D tex;
+            };
+            
+            #define NR_SAMPLERS 16
+            uniform Sampler samplers[NR_SAMPLERS];
+            
+            flat in int TexId;
+            in vec2 TexCoords;
+            
+            out vec4 FragColor;
+            
+            void main()
+            {
+                // FragColor = texture(samplers[TexId].tex, TexCoords);
+                vec4 texColor = texture(samplers[TexId].tex, TexCoords);
+                if (texColor.a < 0.1)
+                {
+                    discard;    
+                }
+                
+                FragColor = texColor;
+            }
+            """))
     {
         ArgumentNullException.ThrowIfNull(resourcesManager);
     }
--- src/MicroEngine/Shaders/MultiTextureSkyboxShader.cs
+++ src/MicroEngine/Shaders/MultiTextureSkyboxShader.cs
@@ -8,8 +8,58 @@ public class MultiTextureSkyboxShader : MultiTextureShaderBase
 {
     public MultiTextureSkyboxShader(IResourcesManager resourcesManager)
         : base(new Shader(
-            resourcesManager.LoadTextFile("Shaders/multi-texture-skybox.vert"),
-            resourcesManager.LoadTextFile("Shaders/multi-texture.frag")))
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            layout (location = 0) in vec3 aPos;
+            layout (location = 1) in float aTexId;
+            layout (location = 2) in vec2 aTexCoords;
+            
+            uniform mat4 model;
+            uniform mat4 view;
+            uniform mat4 projection;
+            
+            flat out int TexId;
+            out vec2 TexCoords;
+            
+            void main()
+            {
+                vec4 position = vec4(aPos, 1.0) * model * view * projection;
+                gl_Position = position.xyww;  // This forces the z value to be 1.0 as it is required to have the skybox "as far as possible".
+                TexId = int(aTexId);
+                TexCoords = aTexCoords;
+            }
+            """,
+            /*language=glsl*/
+            """
+            #version 330 core
+            
+            struct Sampler
+            {
+                sampler2D tex;
+            };
+            
+            #define NR_SAMPLERS 16
+            uniform Sampler samplers[NR_SAMPLERS];
+            
+            flat in int TexId;
+            in vec2 TexCoords;
+            
+            out vec4 FragColor;
+            
+            void main()
+            {
+                // FragColor = texture(samplers[TexId].tex, TexCoords);
+                vec4 texColor = texture(samplers[TexId].tex, TexCoords);
+                if (texColor.a < 0.1)
+                {
+                    discard;    
+                }
+                
+                FragColor = texColor;
+            }
+            """))
     {
         ArgumentNullException.ThrowIfNull(resourcesManager);
     }
