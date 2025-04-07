6c145c5df3942b98a5ce01cbe3598d644469d77d
Premysl Fara <premysl.fara@gmail.com>
2025-04-07 16:41:04 +0200

    WAVE 16bit mono and stereo loader with demo.

---
+++ src/MicroEngine.Audio/Loaders/WaveLoader.cs
@@ -0,0 +1,103 @@
+/* Copyright (C) Premysl Fara and Contributors */
+
+namespace MicroEngine.Audio.Loaders;
+
+/// <summary>
+/// Loads a WAVE file into a Sound instance.
+/// </summary>
+public class WaveLoader
+{
+    public static Sound Load(Stream stream)
+    {
+        if (stream == null)
+        {
+            throw new ArgumentNullException(nameof(stream));
+        }
+
+        using (var reader = new BinaryReader(stream))
+        {
+            // RIFF header
+            var signature = new string(reader.ReadChars(4));
+            if (signature != "RIFF")
+            {
+                throw new NotSupportedException("Specified stream is not a wave file.");
+            }
+
+            var riff_chunck_size = reader.ReadInt32();
+
+            var format = new string(reader.ReadChars(4));
+            if (format != "WAVE")
+            {
+                throw new NotSupportedException("Specified stream is not a wave file.");
+            }
+
+            // WAVE header
+            var format_signature = new string(reader.ReadChars(4));
+            if (format_signature != "fmt ")
+            {
+                throw new NotSupportedException("Specified wave file is not supported.");
+            }
+
+            int format_chunk_size = reader.ReadInt32();
+            int audio_format = reader.ReadInt16();
+            
+            int num_channels = reader.ReadInt16();
+            if (num_channels != 1 && num_channels != 2)
+            {
+                throw new NotSupportedException("Specified wave file is not supported.");
+            }
+            
+            int sample_rate = reader.ReadInt32();
+            if (sample_rate < 1 || sample_rate > 44100)
+            {
+                throw new NotSupportedException("Specified wave file is not supported.");
+            }
+            
+            int byte_rate = reader.ReadInt32();
+            int block_align = reader.ReadInt16();
+            
+            int bits_per_sample = reader.ReadInt16(); 
+            if (bits_per_sample != 16)
+            {
+                throw new NotSupportedException("Specified wave file is not supported.");
+            }
+            
+            var data_signature = new string(reader.ReadChars(4));
+            if (data_signature != "data")
+            {
+                throw new NotSupportedException("Specified wave file is not supported.");
+            }
+
+            var data_chunk_size = reader.ReadInt32();
+            
+            // The sound data
+            var bytes = reader.ReadBytes(data_chunk_size);
+            if (bytes.Length != data_chunk_size)
+            {
+                throw new NotSupportedException("The number of loaded bytes does not match the data chunk size.");
+            }
+            
+            var sound = (num_channels == 1)
+                ? Sound.Create16BitMonoSound(bytes.Length / 2, sample_rate)     // 16 bit mono
+                : Sound.Create16BitStereoSound(bytes.Length / 4, sample_rate);  // 16 bit stereo
+
+            if (num_channels == 1)
+            {
+                for (var i = 0; i < sound.Samples.Length; i++)
+                {
+                    sound.Samples[i] = (short)(bytes[i * 2] | (bytes[i * 2 + 1] << 8));
+                }
+            }
+            else
+            {
+                for (var i = 0; i < sound.Samples.Length; i += 2)
+                {
+                    sound.Samples[i] = (short)(bytes[i * 2] | (bytes[i * 2 + 1] << 8));
+                    sound.Samples[i + 1] = (short)(bytes[i * 2 + 2] | (bytes[i * 2 + 3] << 8));
+                }
+            }
+            
+            return sound;
+        }
+    }
+}
---
+++ src/MicroEngine.Demos.Audio.WaveLoaderDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Copyright (C) Premysl Fara and Contributors */
+
+using MicroEngine.Audio.Loaders;
+
+namespace MicroEngine.Demos.Audio.WaveLoaderDemo;
+
+using OpenTK.Audio.OpenAL;
+
+using MicroEngine.Audio;
+
+/// <summary>
+/// Demonstrates the use of the WAVE sound file loader.
+/// </summary>
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("MicroEngine Audio WAVE Loader Demo");
+        
+        Console.WriteLine();
+
+        // Load the WAVE sound file.
+        Sound? loadedSound;
+        using (var stream = File.OpenRead("Resources/Audio/test_16bit-mono.wav"))
+        {
+            loadedSound = WaveLoader.Load(stream);
+        }
+        
+        // Create the mixer.
+        using (var mixer = new Mixer())
+        {
+            // Initialize the mixer.
+            mixer.Initialize();
+            
+            // Create a sound buffer and attach the sound to it.
+            var buffer = mixer.CreateSoundBuffer();
+            buffer.LoadData(loadedSound);
+            
+            // Create a sound source and attach the sound buffer to it.
+            var source = mixer.CreateSoundSource();
+            source.AttachSoundBuffer(buffer);
+            
+            // Play the sound using the sound source.
+            source.Play();
+            
+            // Wait for the sound to finish playing.
+            while (source.State == ALSourceState.Playing)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        Console.WriteLine();
+        
+        Console.WriteLine("DONE");
+    }
+}
